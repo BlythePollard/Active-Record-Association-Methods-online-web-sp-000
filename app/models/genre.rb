c9bebf0194999aa47edf45c34b08ea4324437d9b
Blythe Pollard <blytheepollard@gmail.com>
2019-11-19 00:51:54 +0000

    Automatically backed up by Learn

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -11,6 +11,7 @@ class Genre < ActiveRecord::Base
   end
 
   def all_artist_names
-    self.artists.first.name.map {|n| n}
+    artist_names = self.artists.first.name
+    artist_names.map {|n| n}
   end
 end
