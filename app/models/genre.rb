84042a968b3aaeabc49f69f4042f5fb4fb31a09b
Blythe Pollard <blytheepollard@gmail.com>
2019-11-19 00:48:10 +0000

    Automatically backed up by Learn

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -11,6 +11,7 @@ class Genre < ActiveRecord::Base
   end
 
   def all_artist_names
-    # return an array of strings containing every musician's name
+    binding.pry
+    self.artists.map do {|t| # return an array of strings containing every musician's name
   end
 end
